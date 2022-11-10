f931f86b7d3749bb1101086c685122a420fae134
yunusfirat <yunusfirat44@gmail.com>
2022-11-10 23:22:39 +0000

    new files has been added.

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -3,7 +3,6 @@ import EventItem from "./event-item";
 import classes from './event-list.module.css'
 function EventList(props){
     const { items } = props;
-console.log(items)
     return (
         <ul className={classes.list}>
             {items.map(event =>(
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,16 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data'
 function EventDetailPage(){
+    const router = useRouter();
+
+const eventId = router.query.eventId;
+const event = getEventById(eventId)
+
+    if(!event){
+        return <p>No Event Found</p>
+    }   
+    
+    
     return (
         <div><h1>Event Detail</h1></div>
     )
