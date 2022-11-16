cf8c5b01709d4643d144ef1f59561be8600ce672
yunusfirat <yunusfirat44@gmail.com>
2022-11-16 12:10:17 +0000

    fetched data from firebase.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
-import { getFeaturedEvents} from '../dummy-data'
+import { getFeaturedEvents } from '../helpers/api-util'
 import EventList from '../components/events/event-list'
-function HomePage() {
-    const feauturedEvents = getFeaturedEvents();
+function HomePage(props) {
     return(
         <div>
-            <EventList items={feauturedEvents}/>
+            <EventList items={props.events}/>
         </div>
     )
+}
+
+export async function getStaticProps(){
+    const feauturedEvents = await getFeaturedEvents();
+    return {
+        props: {
+            events:feauturedEvents
+        }
+    }
 }
 
 export default HomePage;
