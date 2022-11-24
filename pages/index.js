7a8f631cbd2a945da3e2cb43ce689f4063d35714
yunusfirat <yunusfirat44@gmail.com>
2022-11-24 01:05:06 +0000

    progress in head meta and title

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import Head from 'next/head';
+
 import { getFeaturedEvents } from '../helpers/api-util'
 import EventList from '../components/events/event-list'
 function HomePage(props) {
     return(
         <div>
+            <Head>
+                <title>NextJS Events</title>
+                <meta name='description' content='Find a lot of great events that allow you to evolve.'></meta>
+            </Head>
             <EventList items={props.events}/>
         </div>
     )
